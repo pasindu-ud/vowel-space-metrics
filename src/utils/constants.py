3a86e55c04b4237bcc53a8752aa475b36f834e7d
PasinduUd <pasindu.ud@gmail.com>
2026-06-20 07:17:22 +1200

    Add constants.py

---
+++ src/utils/constants.py
@@ -0,0 +1,14 @@
+PLOTLY_TEMPLATE = "plotly_white"
+FONT=dict(family="Times New Roman", size=20)
+
+NZE_FIGURE_HEIGHT = 620
+NZE_FIGURE_WIDTH = 775
+NZE_X_AXIS=dict(title="F2 (Bark)", range=[14.65, 7.6], autorange=False)
+NZE_Y_AXIS=dict(title="F1 (Bark)", range=[7.7, 3.95], autorange=False)
+NZE_LEGEND=dict(orientation="h", x=1, y=1.145, xanchor="right", yanchor="top")
+
+GIE_FIGURE_HEIGHT = 620
+GIE_FIGURE_WIDTH = 775
+GIE_X_AXIS=dict(title="F2 (Bark)", range=[15.4, 7.6], autorange=False)
+GIE_Y_AXIS=dict(title="F1 (Bark)", range=[6.45, 2.5], autorange=False)
+GIE_LEGEND=dict(orientation="h", x=1, y=1.175, xanchor="right", yanchor="top")
